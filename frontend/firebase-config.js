65bd27e690a5f4bdacbf55ce521dd580c35c7760
JuweiLin <92762912+JuweiLin@users.noreply.github.com>
2024-03-03 05:42:29 -0500

    Update firebase-config.js

--- frontend/firebase-config.js
+++ frontend/firebase-config.js
@@ -1,1 +1,10 @@
-
+// firebase-config.js
+export const firebaseConfig = {
+  apiKey: "AIzaSyB_XEM6x5YveXFN3wS2ORseyJTi8U2kEME",
+  authDomain: "city-guide-app-d48a9.firebaseapp.com",
+  projectId: "city-guide-app-d48a9",
+  storageBucket: "city-guide-app-d48a9.appspot.com",
+  messagingSenderId: "181799342059",
+  appId: "1:181799342059:web:96dac38be767878b0693ae",
+  measurementId: "G-XKSVWTR37T"
+};
